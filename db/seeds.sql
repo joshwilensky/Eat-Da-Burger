753a65ac80aca2675e23eccdd519807ac9110e86
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-09 00:15:28 -0400

    Added schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+  burgers (name)
+VALUES
+  ('The Impossible Solution Burger');
+INSERT INTO
+  burgers (name)
+VALUES
+  ('What Shiitake About Burger');
+INSERT INTO
+  burgers (name)
+VALUES
+  ('Top Bun Burger');
+INSERT INTO
+  burgers (name)
+VALUES
+  ('Little Swiss Sunshine Burger');
+INSERT INTO
+  burgers (name)
+VALUES
+  ('The Squeezy Cheesey Bacon Burger');
